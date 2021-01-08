ebd1ddfff72f6f2d6800cb7dcb505087f98b4419
Josh Carter <public@joshcarter.com>
2021-01-08 12:22:05 -0700

    Bed temp avg of input files; remove unnecessary etemp changes.
    - Find mix/max bed temp of the input files. Select output temp halfway
      between them.
    
    - If extruder temp doesn't need to change around M600, don't include
      those lines.

--- gcode_splice.py
+++ gcode_splice.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+#
+# TODO:
+# - 1st layer after change 5C hotter
+#
+
 import argparse
 import re
 import pprint
@@ -18,6 +23,7 @@ print(f'saving to output file: {args.out}')
 
 # Gcode RE's
 extruder_temp = re.compile('^M10[49]\s+[RS](\d+)')
+bed_temp = re.compile('^(M1[49]0)\s+[RS](\d+)')
 fan_speed = re.compile('^M106\s+S(\d+)')
 fan_off = re.compile('^M107')
 filament_change = re.compile('^M600')
@@ -55,6 +61,9 @@ for i in range(len(args.files)):
                 if m := extruder_temp.match(line):
                     # print(f'extruder temp at L{linenum}: {m.groups()[0]}')
                     props[i][section]['extruder_temp'] = int(m.groups()[0])
+                elif m := bed_temp.match(line):
+                    # print(f'bed temp at L{linenum}: {m.groups()[1]}')
+                    props[i][section]['bed_temp'] = int(m.groups()[1])
                 elif m := fan_speed.match(line):
                     # print(f'fan speed at L{linenum}: {m.groups()[0]}')
                     props[i][section]['fan_speed'] = int(m.groups()[0])
@@ -74,6 +83,26 @@ for i in range(len(args.files)):
 
 print(f'pre-scan found properties:')
 pprint.pp(props)
+
+# select bed temp that's average of the min/max of input files
+min_temp = props[0][0]['bed_temp']
+max_temp = min_temp
+
+for file in props:
+    for section in file:
+        if 'bed_temp' in section:
+            bt = section['bed_temp']
+            if bt != 0:
+                if bt < min_temp:
+                    min_temp = bt
+                if bt > max_temp:
+                    max_temp = bt
+
+avg_bed_temp = (min_temp + max_temp) // 2
+print(f'setting bed temp to {avg_bed_temp}C (min {min_temp}, max {max_temp})')
+
+if max_temp >= min_temp + 20:
+    print(f'WARNING: bed temps differ by {max_temp - min_temp}C; may want to select temp manually')
 
 with open(args.out, 'w') as out:
     for i in range(len(args.files)):
@@ -95,6 +124,8 @@ with open(args.out, 'w') as out:
                         file1_props = props[i][section-1]
                         file2_props = props[i+1][section-1]
                         file2_nextprops = props[i+1][section]
+                        etemp1 = file1_props['extruder_temp']
+                        etemp2 = file2_props['extruder_temp']
 
                         # print(f'splice in "{file}" at L{linenum}')
                         # print(f'file1_props i={i} section={section-1}:')
@@ -105,8 +136,8 @@ with open(args.out, 'w') as out:
                         out.write(f'; end {file}\n')
                         out.write('; -------------------- splice begin --------------------\n')
                         # Set extruder temp halfway between file1 and file2
-                        if file1_props['extruder_temp'] > 0 and file2_props['extruder_temp'] > 0:
-                            out.write(f'M104 S{(file1_props["extruder_temp"] + file2_props["extruder_temp"]) // 2} ; extruder temp for filament change\n')
+                        if etemp1 > 0 and etemp2 > 0 and etemp1 != etemp2:
+                            out.write(f'M104 S{(etemp1 + etemp2) // 2} ; extruder temp for filament change\n')
                         out.write('G92 E0.0\n')
                         out.write('G1 E-3.0 ; big retract\n')
                         out.write('G92 E0.0\n')
@@ -120,7 +151,8 @@ with open(args.out, 'w') as out:
                             out.write(f'G1 X{file2_nextprops["xy_move"][0]} Y{file2_nextprops["xy_move"][1]} ; move to where next section begins\n')
                         out.write('M107 ; fan off\n')
                         out.write('M600 ; filament change\n')
-                        out.write(f'M104 S{file2_props["extruder_temp"]} ; extruder temp for next filament\n')
+                        if etemp1 != etemp2:
+                            out.write(f'M104 S{etemp2} ; extruder temp for next filament\n')
                         if file2_props["fan_speed"] > 0:
                             out.write(f'M106 S{file2_props["fan_speed"]} ; fan for next filament\n')
                         out.write(f'M900 K{file2_props["linear_advance"]} ; linear advance for next filament\n')
@@ -128,6 +160,12 @@ with open(args.out, 'w') as out:
                         out.write('; --------------------- splice end ---------------------\n')
                 elif section == i:
                     # Copy over file from this section
-                    out.write(line)
+                    if m := bed_temp.match(line):
+                        if int(m.groups()[1]) > 0:
+                            out.write(f'{m.groups()[0]} S{avg_bed_temp} ; set bed temp\n')
+                        else:
+                            out.write(line) # write temp as-is
+                    else:
+                        out.write(line)
                 elif section > i:
                     break
